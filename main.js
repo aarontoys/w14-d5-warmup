17a87fdd962aa635581533413fef4e4de125649a
Aaron Toys <aarontoys@gmail.com>
2016-04-08 10:12:20 -0600

    1 & 2 done

---
+++ main.js
@@ -0,0 +1,14 @@
+var characters = require('./data/characters.json');
+
+var charArr = characters.data.results
+// 1. Print out every name to the terminal.
+  // charArr.forEach(function (el) {
+  //   console.log(el.name);
+  // })
+
+//2. Get a list of every character and their ID, as an object.
+  // var charObj = {};
+  // charArr.forEach(function (el) {
+  //   charObj[el.name] = el.id;
+  // });
+  // console.log(charObj);
